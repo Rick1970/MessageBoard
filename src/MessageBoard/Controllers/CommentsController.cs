df15baeead18ba522a9acfb234ea32ac7de494ea
rthor <rthor@LAPTOP-343NT6OO>
2016-09-28 16:20:05 -0700

    another redirect from comment create page added.

--- src/MessageBoard/Controllers/CommentsController.cs
+++ src/MessageBoard/Controllers/CommentsController.cs
@@ -34,7 +34,7 @@ namespace MessageBoard.Controllers
         {
             db.Comments.Add(comment);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("../../posts");
         }
         public IActionResult Edit(int id)
         {

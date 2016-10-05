56bfb2e1332b0870bcd0c7aa9eea611ffe9fb1e9
rthor <rthor@LAPTOP-343NT6OO>
2016-10-04 19:02:48 -0700

    added jquery, amd JavaScript files to add interactivity to app.

---
+++ src/MessageBoard/wwwroot/scripts/JavaScript.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    
+    $(".hidden1").fadeIn(1500);
+    $(".hidden1").fadeOut(2500, function () {
+        $('.hidden2').fadeIn(1500);
+        $('.hidden2').fadeOut(2500, function () {
+            $('.hidden3').fadeIn(1500);
+            $('.hidden3').fadeOut(2500, function () {
+                $('.hidden4').fadeIn(1500);
+               
+            });
+            
+        });
+       
+    });
+    
+});

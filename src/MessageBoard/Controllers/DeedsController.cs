07b0ebefe463dd2deefd7a98c38696039caf49dd
rthor <rthor@LAPTOP-343NT6OO>
2016-10-06 13:54:35 -0700

    random pull from db with ajax works.  Currently configured with a button just like the lesson.

--- src/MessageBoard/Controllers/DeedsController.cs
+++ src/MessageBoard/Controllers/DeedsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using MessageBoard.Models;
 using Microsoft.AspNetCore.Authorization;
+using System;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,10 +12,16 @@ namespace MessageBoard.Controllers
 {
     public class DeedsController : Controller
     {
+        private MessageBoardDbContext db = new MessageBoardDbContext();
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult RandomDeedSelector(int count)
+        {
+            var randomDeed = db.Deeds.OrderBy(r => Guid.NewGuid()).Take(count);
+            return Json(randomDeed);
         }
     }
 }

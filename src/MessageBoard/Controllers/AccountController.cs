b60323e519a5e716a837c2a4df7da93c1ab34cd1
rthor <rthor@LAPTOP-343NT6OO>
2016-10-11 15:13:24 -0700

    login corrected.  Returns validation summary, instead of error page.

--- src/MessageBoard/Controllers/AccountController.cs
+++ src/MessageBoard/Controllers/AccountController.cs
@@ -60,15 +60,20 @@ namespace MessageBoard.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
-            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
-            if (result.Succeeded)
+            if (ModelState.IsValid)
             {
-                return RedirectToAction("../home");
+                Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("../home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Could not log in");
+                    return View();
+                }
             }
-            else
-            {
-                return View();
-            }
+            return View(model);
         }
         [HttpPost]
         public async Task<IActionResult> LogOff()
--- src/MessageBoard/ViewModels/LoginViewModel.cs
+++ src/MessageBoard/ViewModels/LoginViewModel.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MessageBoard.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
         public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
